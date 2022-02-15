0ba3d2f19daea976c72cd90ae6fc6a21df21133a
nkey0011 <nkey@stu.jsu.edu>
2022-02-14 21:51:06 -0600

    input fields and guideline

--- app/src/main/java/com/example/nkey_lab2a/MainActivity.java
+++ app/src/main/java/com/example/nkey_lab2a/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.nkey_lab2a;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.text.*;
 import android.os.Bundle;
+import android.view.View;
+import com.example.nkey_lab2a.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
